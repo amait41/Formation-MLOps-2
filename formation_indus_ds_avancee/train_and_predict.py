d6f2d3b7004cf549b160e8035f299a892a74f210
amait41 <adrien.maitammar@gmail.com>
2023-11-09 11:53:34 +0000

    ad mlflow autolog & model logging

--- formation_indus_ds_avancee/train_and_predict.py
+++ formation_indus_ds_avancee/train_and_predict.py
@@ -18,12 +18,10 @@ def train_model(features: pd.DataFrame, model_registry_folder: str) -> None:
     X = features.drop(columns=[target])
     y = features[target]
 
-    mlflow.sklearn.autolog()
     with mlflow.start_run():
-        # insert autolog here ...
+        mlflow.sklearn.autolog()
         model = RandomForestRegressor(n_estimators=1, max_depth=10, n_jobs=1)
         model.fit(X, y)
-    mlflow.sklearn.log_model(model, "model_1")
 
     time_str = time.strftime('%Y%m%d-%H%M%S')
     joblib.dump(model, os.path.join(model_registry_folder, time_str + '.joblib'))
