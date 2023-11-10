fa758c451dd0b4a4799fbc1cef83178956932e50
amait41 <adrien.maitammar@gmail.com>
2023-11-10 15:58:10 +0000

    change model type

--- formation_indus_ds_avancee/train_and_predict.py
+++ formation_indus_ds_avancee/train_and_predict.py
@@ -4,8 +4,7 @@ import time
 import joblib
 import mlflow
 import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
-
+from sklearn.ensemble import GradientBoostingRegressor
 
 def train_model_with_io(features_path: str, model_registry_folder: str) -> None:
     features = pd.read_parquet(features_path)
@@ -19,7 +18,7 @@ def train_model(features: pd.DataFrame, model_registry_folder: str) -> None:
     y = features[target]
     with mlflow.start_run():
         mlflow.sklearn.autolog(log_models=True)
-        model = RandomForestRegressor(n_estimators=1, max_depth=10, n_jobs=1)
+        model = GradientBoostingRegressor(n_estimators=10, n_jobs=1)
         model.fit(X, y)
     time_str = time.strftime('%Y%m%d-%H%M%S')
     joblib.dump(model, os.path.join(model_registry_folder, time_str + '.joblib'))
